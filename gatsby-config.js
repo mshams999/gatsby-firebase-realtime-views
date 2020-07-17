1f6f2c017772e0c2162714ea22d8dc19f35b8071
mohamed <shamsmohamed155@gmail.com>
2020-07-17 21:32:21 +0200

    view

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,4 @@
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
+require("dotenv").config({ path: ".env" })
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
